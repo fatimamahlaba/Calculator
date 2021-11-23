ee91606735b8214ea4227ef2ee589e9ed5af2b33
fatima <fatimamahlaba@gmail.com>
2021-11-23 15:14:42 +0200

    culc

---
+++ myscript.js
@@ -0,0 +1,15 @@
+let sum = "";
+
+function addToSum(operator) {
+    sum = sum + operator;
+    document.getElementById("output").innerText = sum;
+}
+
+function evaluateSum() {
+    document.getElementById("output").innerText = eval(sum);
+}
+function clearSum() {
+    sum = "";
+    document.getElementById("output").innerText = sum;
+}
+
